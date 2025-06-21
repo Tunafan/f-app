d5d9ecf7e3cdeb3b9943465c14a5d0b4d97e5146
Ziggy L <trollebanditten@gmail.com>
2025-06-21 16:19:17 +0200

    Revert "Merge branch 'develop' into Initial-project-setup-ZIGGY"
    This reverts commit 6bdf17aae0bdaab055e02f6c7396357b261a1d17, reversing
    changes made to 7ec026ea46a15f0746af9641db865196896e1c9a.

--- src/boot/localStorage.js
+++
@@ -1,5 +0,0 @@
-// Initialize auth from localStorage to prevent need for hard refreshes on Firebase
-export default async ({ store } /* { app, router, Vue ... } */) => {
-  store.dispatch('auth/initializeStore', null, { root: true });
-  store.dispatch('region/initializeStore', null, { root: true });
-};
